d2ee6773f63ed5782a8456cf63d8b6c94d733298
Andreas Janzen <janzen@gmx.net>
2020-07-19 18:23:26 +0200

    Added rudimentary gitprofiler.py version.

---
+++ gitprofiler.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+gitprofiler.py
+
+Author: Dr. Andreas Janzen, janzen@gmx.net
+Date: 2020-07-15
+"""
+
+import os
+import sys
+
+from github import Github
+
+
+gh = Github()
+print("Jetzt habe ich irgendwelchen Quatsch eingefügt.")
+
+user = gh.get_user("aojanzen")
